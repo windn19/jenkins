f76616616022b5dea90b28e25072b2096a420951
dmitry <windn12@gmail.com>
2024-09-28 19:07:22 +0500

    add directory lab2

--- lab2/train_model.py
+++ lab2/train_model.py
@@ -4,11 +4,11 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 
-data_train = pd.read_csv('data_train.csv')
+data_train = pd.read_csv('../data_train.csv')
 X_train = data_train[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']].values
 y_train = data_train['Survived'].values
 
 model = LogisticRegression(max_iter=100_000).fit(X_train, y_train)
 
-with open('model.pkl', mode='bw') as f:
+with open('../model.pkl', mode='bw') as f:
     pickle.dump(model, f)
